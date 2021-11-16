60017ef463e613ba8732ae0825eefa517495b8c4
Leonardo Herrera <leo2399@gmail.com>
2021-11-16 01:25:03 -0500

    Created errors messages for serial and name

--- src/main/java/baseline/InventoryController.java
+++ src/main/java/baseline/InventoryController.java
@@ -4,6 +4,9 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.math.BigDecimal;
 import java.text.NumberFormat;
@@ -53,9 +56,12 @@ public class InventoryController {
     @FXML
     private TextField valueTextField;
 
+    @FXML
+    private AnchorPane pane;
+
     private final ObservableList<Items> list = FXCollections.observableArrayList();
     private final NumberFormat currency = NumberFormat.getCurrencyInstance();
-
+    private Alert errorMessage = new Alert(Alert.AlertType.ERROR);
     @FXML
     void addItem(ActionEvent event) {
         // Add an item to the inventory list
@@ -66,11 +72,32 @@ public class InventoryController {
         list.add(new Items(serialTextField.getText(), nameTextField.getText(), value));
         inventory.setItems(list);
 
-        // Add the serial number
-        serialNumCol.setCellValueFactory(param -> param.getValue().serialNumProperty());
+        // Check serial number for the correct format
+        if(serialTextField.getText().matches("[A-Z][-][A-Z0-9]{3}[-][A-Z0-9]{3}[-][A-Z0-9]{3}")){
 
-        // Add the name
-        nameCol.setCellValueFactory(param -> param.getValue().nameProperty());
+            // Add the serial number
+            serialNumCol.setCellValueFactory(param -> param.getValue().serialNumProperty());
+        }else{
+
+            // Error message if the serial number not in the correct format
+            errorMessage.setHeaderText("Serial Number Error");
+            errorMessage.setContentText("Serial number must be in A-XXX-XXX-XXX format\n" +
+                   "A must be a letter and X can be either a letter or a digit");
+            errorMessage.showAndWait();
+        }
+
+        // Checks to make sure name isn't blank and is within 2 to 256 characters
+        if(nameTextField.getText().isEmpty() || nameTextField.getText().length() < 2 || nameTextField.getText().length() > 256){
+
+            // Error message if name is doesn't meet the requirements
+            errorMessage.setHeaderText("Name Error");
+            errorMessage.setContentText("Name must be between 2 to 256 characters in length");
+            errorMessage.showAndWait();
+        }else{
+            
+            // Add the name
+            nameCol.setCellValueFactory(param -> param.getValue().nameProperty());
+        }
 
         // Add the value
         valueCol.setCellValueFactory(param -> param.getValue().valueProperty());
