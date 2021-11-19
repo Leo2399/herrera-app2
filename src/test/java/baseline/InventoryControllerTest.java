ced94bf07f4c40fcccbbd3fefeb9dffb10076653
Leonardo Herrera <leo2399@gmail.com>
2021-11-19 03:23:44 -0500

    new test

--- src/test/java/baseline/InventoryControllerTest.java
+++ src/test/java/baseline/InventoryControllerTest.java
@@ -9,15 +9,24 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InventoryControllerTest {
 
-    private Items expected = new Items("A-aaa-11-22", "Name", "$20.00");
+    private Items expected = new Items("A-1C2-5GG-1K5", "PS5", "$599.99");
 
     @Test
     void addItem() {
+        String number = "A-1C2-5GG-1K5";
+        String name = "PS5";
+        String value = "$599.99";
+
+        assertEquals(number, expected.getSerialNum());
+        assertEquals(name, expected.getName());
+        assertEquals(value, expected.getValue());
+
+        System.out.println();
     }
 
     @Test
     void testSaveToFile(){
         System.out.println(expected);
-        assertEquals("A-aaa-11-22 Name $20.00", expected.toString());
+        assertEquals("A-1C2-5GG-1K5\tPS5\t$599.99", expected.toString());
     }
 }
