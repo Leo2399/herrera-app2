e5f15548b050a4b414fc025b82f88c3e6f8207cf
Leonardo Herrera <leo2399@gmail.com>
2021-11-21 17:48:55 -0500

    added more tests

--- src/test/java/baseline/InventoryControllerTest.java
+++ src/test/java/baseline/InventoryControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,16 +17,18 @@ class InventoryControllerTest {
     private final Items expected = new Items("A-1C2-5GG-1K5", "PS5", "$599.99");
     private final ObservableList<Items> list = FXCollections.observableArrayList();
 
-
     @Test
     void addItem() {
-        String number = "A-1C2-5GG-1K5";
-        String name = "PS5";
-        String value = "$599.99";
 
-        assertEquals(number, expected.getSerialNum());
-        assertEquals(name, expected.getName());
-        assertEquals(value, expected.getValue());
+        String number = expected.getSerialNum();
+        String name = expected.getName();
+        String value = expected.getValue();
+
+        list.add(expected);
+
+        assertEquals(number, list.get(0).getSerialNum());
+        assertEquals(name, list.get(0).getName());
+        assertEquals(value, list.get(0).getValue());
 
         System.out.println(number+" = "+expected.getSerialNum());
         System.out.println(name+" = "+expected.getName());
@@ -35,20 +38,43 @@ class InventoryControllerTest {
     @Test
     void deleteItem(){
 
+        int listSize = 3;
+
         list.add(new Items("A-1C2-5GG-1K5", "PS5", "$599.99"));
         list.add(new Items("B-123-456-747", "Something", "$69.00"));
         list.add(new Items("C-D84-GGG-LOL", "Please give me a C", "$1.00"));
 
-        list.remove(0);
+        System.out.println("Number of items: "+list.size());
 
-        assertEquals(2, list.size());
+        list.remove(0);
+        assertEquals(listSize-1, list.size());
 
         System.out.println("An item was removed, items left: "+list.size());
     }
 
     @Test
+    void clearItems(){
+        int listSize = 3;
+
+        list.add(new Items("A-1C2-5GG-1K5", "PS5", "$599.99"));
+        list.add(new Items("B-123-456-747", "Something", "$69.00"));
+        list.add(new Items("C-D84-GGG-LOL", "Please give me a C", "$1.00"));
+
+        System.out.println("Number of items: "+list.size());
+        assertEquals(listSize, list.size());
+
+        list.clear();
+
+        assertEquals(listSize-3, list.size());
+        System.out.println("Cleared list of all the items");
+    }
+
+    @Test
     void testMinItems(){
+
         int minItems = 1024;
+
+        System.out.println("Total items (before adding): "+list.size());
 
         for(int i=0; i<minItems; i++){
             list.add(expected);
@@ -60,9 +86,60 @@ class InventoryControllerTest {
     }
 
     @Test
-    void testSaveToFile(){
-        System.out.println(expected);
-        assertEquals("A-1C2-5GG-1K5\tPS5\t$599.99", expected.toString());
+    void testSearchBySerialNumber(){
+        ObservableList<Items> search = FXCollections.observableArrayList();
+        int expectedSize = 1;
+
+        list.add(new Items("A-1C2-5GG-1K5", "PS5", "$599.99"));
+        list.add(new Items("B-123-456-747", "Something", "$69.00"));
+        list.add(new Items("C-D84-GGG-LOL", "Please give me a C", "$1.00"));
+
+        for(Items i : list){
+            String lowerCaseFilter = i.getSerialNum().toLowerCase(Locale.ROOT);
+            if(lowerCaseFilter.contains("a")){
+                search.add(i);
+            }
+        }
+
+        assertEquals(expectedSize, search.size());
+        System.out.println("Number of items matching search bar: "+search.size());
+    }
+
+    @Test
+    void testSearchByName(){
+        ObservableList<Items> search = FXCollections.observableArrayList();
+        int expectedSize = 2;
+
+        list.add(new Items("A-1C2-5GG-1K5", "PS5", "$599.99"));
+        list.add(new Items("B-123-456-747", "Something", "$69.00"));
+        list.add(new Items("C-D84-GGG-LOL", "Please give me a C", "$1.00"));
+
+        for(Items i : list){
+            String lowerCaseFilter = i.getName().toLowerCase(Locale.ROOT);
+            if(lowerCaseFilter.contains("p")){
+                search.add(i);
+            }
+        }
+
+        assertEquals(expectedSize, search.size());
+        System.out.println("Number of items matching search bar: "+search.size());
+    }
+
+    @Test
+    void testSaveToFile() throws IOException {
+        FileManagement testSave = new FileManagement();
+        list.add(new Items("A-1C2-5GG-1K5", "PS5", "$599.99"));
+        list.add(new Items("B-123-456-747", "Something", "$69.00"));
+        list.add(new Items("C-D84-GGG-LOL", "Please give me a C", "$1.00"));
+
+        testSave.save(list, new File("C:\\temp/testCase.txt"));
+
+        int expectedSize = 3;
+        System.out.println("File created");
+
+        testSave.load(list, new File("C:\\temp/testCase.txt"));
+        assertEquals(expectedSize, list.size());
+        System.out.println("Size of new file: "+list.size());
     }
 
     @Test
@@ -75,4 +152,5 @@ class InventoryControllerTest {
 
         System.out.println("Size of loaded list: " +list.size());
     }
+
 }
